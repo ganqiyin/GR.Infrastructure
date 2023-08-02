147e21fd62757296acf9c29f46e5ba5e67ae6d3a
ganqiyin <qiyin.gan@kinwong.com>
2023-08-02 16:22:22 +0800

    添加nacos

---
+++ src/GR.Nacos/Nacos/Extension.Nacos.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nacos.AspNetCore.V2;
+
+namespace GR.Nacos
+{
+    public static partial class Extension
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks>
+        /// https://www.cnblogs.com/wucy/p/13230453.html
+        /// https://www.cnblogs.com/buruainiaaaa/p/14121176.html
+        /// </remarks>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <param name="section"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddNacos(this IServiceCollection services, IConfiguration configuration, string section = "nacos")
+        {
+            services.AddNacosAspNet(configuration, section: section);
+            return services;
+        }
+    }
+}
---
+++ src/GR.Nacos/Nacos/NacosConst.cs
@@ -0,0 +1,16 @@
+﻿namespace GR.Nacos
+{
+    /// <summary>
+    /// 常量
+    /// </summary>
+    public class NacosConst
+    {
+        public class Http
+        {
+            /// <summary>
+            /// 客户端名称
+            /// </summary>
+            public const string ClientName = "NacosHttpClient";
+        }
+    }
+}
