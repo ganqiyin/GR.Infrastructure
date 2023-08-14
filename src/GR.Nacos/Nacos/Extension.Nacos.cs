30a4e96fdf71d433ef0b1da43fdd69653640233d
ganqiyin <qiyin.gan@kinwong.com>
2023-08-14 11:17:22 +0800

    完成 nacos 集成

--- src/GR.Nacos/Nacos/Extension.Nacos.cs
+++ src/GR.Nacos/Nacos/Extension.Nacos.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Nacos.AspNetCore.V2;
+using Nacos.V2.DependencyInjection;
 
 namespace GR.Nacos
 {
@@ -10,6 +11,7 @@ namespace GR.Nacos
         /// 
         /// </summary>
         /// <remarks>
+        /// https://nacos-sdk-csharp.readthedocs.io/en/latest/introduction/gettingstarted.html
         /// https://www.cnblogs.com/wucy/p/13230453.html
         /// https://www.cnblogs.com/buruainiaaaa/p/14121176.html
         /// </remarks>
@@ -17,9 +19,12 @@ namespace GR.Nacos
         /// <param name="configuration"></param>
         /// <param name="section"></param>
         /// <returns></returns>
-        public static IServiceCollection AddNacos(this IServiceCollection services, IConfiguration configuration, string section = "nacos")
+        public static IServiceCollection AddNacos(this IServiceCollection services, IConfigurationBuilder configBuilder, IConfiguration configuration, string section = "nacos")
         {
+            // 注册服务到Nacos
             services.AddNacosAspNet(configuration, section: section);
+            //添加配置中心:默认会使用JSON解析器来解析存在Nacos Server的配置
+            configBuilder.AddNacosV2Configuration(configuration.GetSection(section));
             return services;
         }
     }
--- src/GR.Nacos/Nacos/Handlers/NacosDiscoveryDelegatingHandler.cs
+++ src/GR.Nacos/Nacos/Handlers/NacosDiscoveryDelegatingHandler.cs
@@ -3,37 +3,37 @@ using Nacos.V2;
 
 namespace GR.Nacos.Handlers
 {
-    public class NacosDiscoveryDelegatingHandler : DelegatingHandler
-    {
-        private readonly ILogger _logger;
-        private readonly INacosNamingService _nameSrv;
+    //public class NacosDiscoveryDelegatingHandler : DelegatingHandler
+    //{
+    //    private readonly ILogger _logger;
+    //    private readonly INacosNamingService _nameSrv;
 
-        public NacosDiscoveryDelegatingHandler(ILogger<NacosDiscoveryDelegatingHandler> logger
-            , INacosNamingService nameSrv)
-        {
-            _logger = logger;
-            _nameSrv = nameSrv;
-        }
+    //    public NacosDiscoveryDelegatingHandler(ILogger<NacosDiscoveryDelegatingHandler> logger
+    //        , INacosNamingService nameSrv)
+    //    {
+    //        _logger = logger;
+    //        _nameSrv = nameSrv;
+    //    }
 
-        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-        {
-            //https://blog.csdn.net/wuchsh123/article/details/124921566
-            //var currentRequest = request.RequestUri;
-            //try
-            //{
-            //    var instance = await _nameSrv.SelectOneHealthyInstance(currentRequest.Host);
-            //    //var host=string.Format("{}")
-            //    //request.RequestUri=new Uri(string.Format("{0}{1}",instance.ServiceName))
-            //}
-            //catch (Exception ex)
-            //{
+    //    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    //    {
+    //        //https://blog.csdn.net/wuchsh123/article/details/124921566
+    //        //var currentRequest = request.RequestUri;
+    //        //try
+    //        //{
+    //        //    var instance = await _nameSrv.SelectOneHealthyInstance(currentRequest.Host);
+    //        //    //var host=string.Format("{}")
+    //        //    //request.RequestUri=new Uri(string.Format("{0}{1}",instance.ServiceName))
+    //        //}
+    //        //catch (Exception ex)
+    //        //{
 
-            //}
-            //finally
-            //{
+    //        //}
+    //        //finally
+    //        //{
 
-            //}
-            return base.SendAsync(request, cancellationToken);
-        }
-    }
+    //        //}
+    //        return base.SendAsync(request, cancellationToken);
+    //    }
+    //}
 }
